bb1aa29ec6b7e5ac9194c522c622fe8985601a67
Schasltish <aiza.zhalieva@mail.ru>
2026-04-07 23:35:35 +0300

    Update 20260406183459_InitialCreate.Designer.cs

--- Migrations/20260406183459_InitialCreate.Designer.cs
+++ Migrations/20260406183459_InitialCreate.Designer.cs
@@ -20,7 +20,7 @@ namespace TestingProlect.Migrations
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.5");
 
-            modelBuilder.Entity("TestingProject.Models.TestResult", b =>
+            modelBuilder.Entity("TestingProlect.Models.TestResult", b =>
                 {
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
