e8e64c8bfc9ea8c9548cde1400cab8ee02df4fe8
Schasltish <aiza.zhalieva@mail.ru>
2026-04-09 21:16:48 +0300

    Update AccountController.cs

--- AccountController.cs
+++ AccountController.cs
@@ -1,1 +1,27 @@
 
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+
+public class AccountController : Controller
+{
+    // Здесь мы подключаем твою базу данных (ApplicationDbContext)
+    // чтобы проверять логины и пароли
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Login(string username, string password)
+    {
+        // Простая проверка: есть ли такой пользователь в таблице "users"
+        // В будущем здесь добавим проверку ролей (Преподаватель/Студент)
+        return RedirectToAction("Index", "Home");
+    }
+
+    public IActionResult Register()
+    {
+        return View();
+    }
+}
