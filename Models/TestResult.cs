a25a70dbc6303c084acf53bfb0e8e69243203cb5
Schasltish <aiza.zhalieva@mail.ru>
2026-04-10 13:47:28 +0300

    Update TestResult.cs

--- Models/TestResult.cs
+++ Models/TestResult.cs
@@ -1,12 +1,12 @@
 using System;
 
-namespace TestingProject.Models
+namespace TestingProlect.Models
 {
     public class TestResult
     {
         public int Id { get; set; }
-        public string StudentName { get; set; }
-        public string GroupName { get; set; }
+        public string StudentName { get; set; } = string.Empty;
+        public string GroupName { get; set; } = string.Empty;
         public int Score { get; set; }
         public int TotalQuestions { get; set; }
         public DateTime TestDate { get; set; } = DateTime.Now;
