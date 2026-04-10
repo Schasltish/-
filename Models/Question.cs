93ad45b5177b5df1dcae82c672630abb47e421ba
Schasltish <aiza.zhalieva@mail.ru>
2026-04-10 11:46:15 +0300

    Update Question.cs

--- Models/Question.cs
+++ Models/Question.cs
@@ -1,12 +1,15 @@
-namespace TestingProject.Models
+namespace YourProject.Models
 {
     public class Question
     {
         public int Id { get; set; }
-        public string Text { get; set; }
-        public string Answer { get; set; } // Правильный ответ
+
         public int TestId { get; set; }
-        public Test Test { get; set; }
+
+        public Test Test { get; set; } = null!;
+
+        public string Text { get; set; } = string.Empty;
+
+        public List<AnswerOption> Options { get; set; } = new();
     }
 }
-
