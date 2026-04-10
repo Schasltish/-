49630d17e999d3b72af228c6a2e6c4f3ef7c333a
Schasltish <aiza.zhalieva@mail.ru>
2026-04-10 13:27:18 +0300

    Update CreateTest.cshtml.cs

--- Pages/CreateTest.cshtml.cs
+++ Pages/CreateTest.cshtml.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using TestingProject.Data; 
+using TestingProject.Data;
 using TestingProject.Models;
 using System.Threading.Tasks;
 
@@ -15,14 +15,28 @@ namespace TestingProject.Pages
             _context = context;
         }
 
+        [BindProperty]
+        public string TestName { get; set; } = string.Empty;
+
+        [BindProperty]
+        public string Description { get; set; } = string.Empty;
+
         public void OnGet() { }
 
-        public async Task<IActionResult> OnPostAsync(string TestName, string Description)
+        public async Task<IActionResult> OnPostAsync()
         {
-            var newTest = new Test { Title = TestName, Description = Description };
+            if (!ModelState.IsValid) return Page();
+
+            var newTest = new Test 
+            { 
+                Title = TestName, 
+                Description = Description 
+            };
+
             _context.Tests.Add(newTest);
             await _context.SaveChangesAsync();
 
+            // Перенаправляем на главную
             return RedirectToPage("./Index");
         }
     }
