52ab5eac14af172655cd410df655beb421b3bbd3
Schasltish <aiza.zhalieva@mail.ru>
2026-04-10 13:22:32 +0300

    Update AnswerOption.cs

--- Models/AnswerOption.cs
+++ Models/AnswerOption.cs
@@ -1,15 +1,11 @@
-namespace YourProject.Models
+namespace TestingProject.Models
 {
     public class AnswerOption
     {
         public int Id { get; set; }
-
         public int QuestionId { get; set; }
-
         public Question Question { get; set; } = null!;
-
         public string Text { get; set; } = string.Empty;
-
         public bool IsCorrect { get; set; }
     }
 }
