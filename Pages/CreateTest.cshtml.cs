2e1789df38b7a9ba1a36dc65b5fad1387625bd3b
Schasltish <aiza.zhalieva@mail.ru>
2026-04-10 09:30:23 +0300

    Update CreateTest.cshtml.cs

--- Pages/CreateTest.cshtml.cs
+++ Pages/CreateTest.cshtml.cs
@@ -1,1 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using YourProjectName.Data; // Замени на свое пространство имен
+using YourProjectName.Models;
+
+public class CreateTestModel : PageModel
+{
+    private readonly ApplicationDbContext _context;
+
+    public CreateTestModel(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public void OnGet() { }
+
+    public async Task<IActionResult> OnPostAsync(string TestName, string Description)
+    {
+        var newTest = new Test { Title = TestName, Description = Description };
+        _context.Tests.Add(newTest);
+        await _context.SaveChangesAsync();
+
+        // После создания теста перекидываем на добавление вопросов к нему
+        return RedirectToPage("./AddQuestions", new { id = newTest.Id });
+    }
+}
 
