ecff21254e6546e415878e956f70c80a2d47117e
Schasltish <aiza.zhalieva@mail.ru>
2026-04-10 13:20:39 +0300

    Update Question.cs

--- Models/Question.cs
+++ Models/Question.cs
@@ -1,15 +1,13 @@
-namespace YourProject.Models
+using System.Collections.Generic;
+
+namespace TestingProject.Models
 {
     public class Question
     {
         public int Id { get; set; }
-
         public int TestId { get; set; }
-
         public Test Test { get; set; } = null!;
-
         public string Text { get; set; } = string.Empty;
-
         public List<AnswerOption> Options { get; set; } = new();
     }
 }
