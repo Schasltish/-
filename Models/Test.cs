9dbe291025f50c60da9f2ba06d84126f58fe48d3
Schasltish <aiza.zhalieva@mail.ru>
2026-04-10 13:20:19 +0300

    Update Test.cs

--- Models/Test.cs
+++ Models/Test.cs
@@ -1,15 +1,14 @@
-namespace YourProject.Models
+using System;
+using System.Collections.Generic;
+
+namespace TestingProject.Models
 {
     public class Test
     {
         public int Id { get; set; }
-
         public string Title { get; set; } = string.Empty;
-
         public string Description { get; set; } = string.Empty;
-
         public DateTime CreatedAt { get; set; } = DateTime.Now;
-
         public List<Question> Questions { get; set; } = new();
     }
 }
