ce17e5822695e0a32695acc07a780d325b4d1d15
Schasltish <aiza.zhalieva@mail.ru>
2026-04-10 09:45:26 +0300

    Update TestResult.cs

--- Models/TestResult.cs
+++ Models/TestResult.cs
@@ -1,50 +1,14 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using TestingProject.Models;
-using TestingProject.Data; // Убедись, что папка с контекстом называется Data
-using System.Threading.Tasks;
+using System;
 
-namespace TestingProject.Pages
+namespace TestingProject.Models
 {
-    public class CreateTestModel : PageModel
+    public class TestResult
     {
-        private readonly ApplicationDbContext _context;
-
-        public CreateTestModel(ApplicationDbContext context)
-        {
-            _context = context;
-        }
-
-        [BindProperty]
-        public string TestName { get; set; }
-
-        [BindProperty]
-        public string Description { get; set; }
-
-        public void OnGet()
-        {
-        }
-
-        public async Task<IActionResult> OnPostAsync()
-        {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
-
-            // Создаем новый объект теста
-            // ВАЖНО: Проверь, как называются поля в твоем файле Test.cs
-            var newTest = new Test 
-            { 
-                Title = TestName, 
-                Description = Description 
-            };
-
-            _context.Tests.Add(newTest);
-            await _context.SaveChangesAsync();
-
-            // Перенаправляем на страницу добавления вопросов к этому тесту
-            return RedirectToPage("./Index"); 
-        }
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+        public string GroupName { get; set; }
+        public int Score { get; set; }
+        public int TotalQuestions { get; set; }
+        public DateTime TestDate { get; set; } = DateTime.Now;
     }
 }
